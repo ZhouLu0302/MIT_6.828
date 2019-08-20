65492ecb48a20beae92ed6d0436f2d0ef15b8f89
ZhouLu0302 <zhoulu0302@gmail.com>
2019-08-20 22:06:45 +0800

    finish lab6 exercise4

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,9 +3,16 @@
 
 // LAB 6: Your driver code here
 // Add by Zhou
+
+volatile uint32_t *bar_va;
+
 int pci_e1000_attach(struct pci_func *pcif)
 {
     pci_func_enable(pcif);
+    bar_va = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+
+    cprintf("e1000: bar0 %x, size0 %x\n", pcif->reg_base[0], pcif->reg_size[0]);
+    cprintf("e1000: status %x\n", bar_va[2]);
 
     return 1;
 }
